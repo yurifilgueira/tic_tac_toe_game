af1eb97733dfdc08cd5619a1a033093cec5c9871
Yuri FTM <yurimedeiros141@gmail.com>
2024-03-27 00:10:43 -0300

    Started game logic implementation
    Created basic board structure. Added functionality for player to select a cell (currently in testing phase)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
+use std::io::{self};
+
 fn main() {
     
-    let board: [[char; 3]; 3];
+    let mut board: [[char; 3]; 3] = [[' '; 3]; 3];
+
+    while true {
+
+        println!(" {} | {} | {}", board[0][0], board[0][1], board[0][2]);
+        println!("-----------");
+        println!(" {} | {} | {}", board[1][0], board[1][1], board[1][2]);
+        println!("-----------");
+        println!(" {} | {} | {}", board[2][0], board[2][1], board[2][2]);
+
+        let mut position: String = String::new();
+        io::stdin().read_line(&mut position).expect("Fail to read input");
+
+        let position: i32 = match position.trim().parse::<i32>() {
+            Ok(numb) => {numb},
+            Err(_) => {
+                println!("Invalid input.");
+                continue;
+            }
+        };
+
+        calculate_position(&mut board, position);
+    
+    }
+
+}
+
+fn calculate_position(board: &mut [[char; 3]; 3], position: i32){
+
+    if position == 1 {
+        board[0][0] = 'X';
+    }
 
 }
